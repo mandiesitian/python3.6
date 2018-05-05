e6a31ac4340fc27d29a4edca6f3b533e6a9e1c39
mandiesitian <34275083+mandiesitian@users.noreply.github.com>
2018-05-05 21:19:56 +0800

    move  改

--- GUI/move.py
+++ GUI/move.py
@@ -12,7 +12,7 @@ p3=50,35;
 r=canves.create_polygon(p1,p2,p3,fill='yellow',outline='blue');
 #r  创建三角形实例
 for i in range(0,25):
-    canves.move(r,5,0);   #id 为 的人的实例 向右移动5个像素  向下移动0个像素
+    canves.move(r,5,0);   #id 为 的的实例 向右移动5个像素  向下移动0个像素
     root.update();        #重新绘制画布
-    time.sleep(0.05);     #使程序睡眠0.05秒后 在执行
+    time.sleep(0.05);     #使程序睡眠0.05秒后 再执行
 root.mainloop();
