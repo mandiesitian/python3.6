dbb4d1f83c538fb64d2fb21dcdc8322ac01aa6ba
mandiesitian <34275083+mandiesitian@users.noreply.github.com>
2018-05-05 20:27:45 +0800

    GUI主窗口

---
+++ GUI/GUI1.py
@@ -0,0 +1,13 @@
+#_*_ coding:utf-8 _*_
+# 导入tkinter
+# 创建GUI主窗口
+# 添加各种组件
+# 使用mainloop（）函数进入时间主循环
+
+
+
+import tkinter as tk;
+root=tk.Tk();
+root.resizable(1,1);
+root.geometry('500x300');
+root.mainloop();
