bb8ef65eabe0c97cdb2e3a53422dbdb444739ec6
mandiesitian <34275083+mandiesitian@users.noreply.github.com>
2018-05-05 20:34:39 +0800

    Button组件

--- GUI/GUI1.py
+++ GUI/GUI1.py
@@ -4,10 +4,15 @@
 # 添加各种组件
 # 使用mainloop（）函数进入时间主循环
 
+#控制组件在界面中显示的位置需要布局管理器 pack（）方法就是将组件包装
+#到一个父组件中 用于创建一个版面 还有grid（） place（）
 
 
 import tkinter as tk;
 root=tk.Tk();
+root.title('top window');  #加入标题
 root.resizable(1,1);
 root.geometry('500x300');
+tk.Button(root,text='save').pack();  #加入Button
+tk.Button(root,text='cancle').pack();
 root.mainloop();
