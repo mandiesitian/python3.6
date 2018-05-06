b455cf654f877e57a3cd17475de1330848a6de46
mandiesitian <34275083+mandiesitian@users.noreply.github.com>
2018-05-06 22:44:54 +0800

    BallGame4

--- GUI/BallGame.py
+++ GUI/BallGame.py
@@ -32,7 +32,7 @@ class Ball:
         #获取画布当前宽度
     def draw(self):
         self.canvas.move(self.id,self.x,self.y);
-        pos=self.canvas.coords(self.id);   #coords返回画布上画好的x，y坐标  列表  上下左右
+        pos=self.canvas.coords(self.id);   #coords返回画布上画好的x，y坐标  列表  左上右下
 
 
         if(pos[1]<=0):
@@ -43,11 +43,55 @@ class Ball:
             self.x=3;
         if(pos[2]>=self.canvas_width):
             self.x=-3;
+class Paddle:
+    def __init__(self,canvas,color):
+        self.canvas=canvas;
+        self.id=canvas.create_rectangle(0,0,150,10,fill=color);
+        self.canvas.move(self.id,150,380);
+
+        self.start=False;
+        self.x=0;
+        self.canvas_width=self.canvas.winfo_width();
+
+        self.canvas.bind_all('<KeyPress-Left>',self.turn_left);
+        self.canvas.bind_all('<KeyPress-Right>',self.turn_right);
+        self.canvas.bind_all('<KeyPress-Up>',self.game_start);
+        self.canvas.bind_all('<KeyPress-Down>',self.game_stop);
+
+    def turn_left(self,event):
+        self.x=-4;
+
+    def turn_right(self,event):
+        self.x=4;
+    def game_start(self,event):
+        self.start=True;
+    def game_stop(self,event):
+        self.start=False;
+
+    def draw(self):
+        self.canvas.move(self.id,self.x,0);
+        pos=self.canvas.coords(self.id);
+
+
+        if(pos[0]<=0):
+            self.x=0;
+        if (pos[2] >= 500):
+            self.x = 0;
+
+
+
+
+
 
 ball=Ball(canvas,'red');
+paddle=Paddle(canvas,'blue')
 
 while 1:
-    ball.draw();
+    # ball.draw();
+    if(paddle.start==True):
+        paddle.draw();
+        ball.draw();
+    root.update_idletasks();
     root.update();
     time.sleep(0.01);
 
@@ -57,4 +101,4 @@ while 1:
 
 
 
-# root.mainloop();
+
