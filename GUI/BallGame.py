cffa3988d6cddee4d377e2ce1c04c8c3c238340e
mandiesitian <34275083+mandiesitian@users.noreply.github.com>
2018-05-06 21:52:50 +0800

    BallGame2

--- GUI/BallGame.py
+++ GUI/BallGame.py
@@ -2,18 +2,21 @@
 #弹球游戏
 from tkinter import *;
 import random;
+import time;
 root=Tk();
+root.wm_attributes('-topmost',1);
+#使画布置于所有窗口之前
 root.resizable(0,0);
-root.geometry('500x400');
+# root.geometry('500x400');
 root.title('BallGame');
-canvas=Canvas(root,width=500,height=400,bg='blue',bd=0);
+canvas=Canvas(root,width=500,height=400,bg='blue',bd=0,highlightthickness=0);
 canvas.pack();
 root.update();
 
 class Ball:
     def __init__(self,canvas,color):
         self.canvas=canvas;
-        self.id=canvas.create_oval(10,10,25,25,fill='color');
+        self.id=canvas.create_oval(10,10,25,25,fill=color);
         self.canvas.move(self.id,245,100);
 
         start=[-3,-2,-1,1,2,3];
@@ -25,9 +28,31 @@ class Ball:
         #获取画布当前高度
         self.canvas_width = self.canvas.winfo_width();
         #获取画布当前宽度
+    def draw(self):
+        self.canvas.move(self.id,self.x,self.y);
+        pos=self.canvas.coords(self.id);   #coords返回画布上画好的x，y坐标  列表  上下左右
+
+
+        if(pos[1]<=0):
+            self.y=3;
+        if(pos[3]>=self.canvas_height):
+            self.y=-3;
+        if(pos[0]<=0):
+            self.x=3;
+        if(pos[2]>=self.canvas_width):
+            self.x=-3;
+
+ball=Ball(canvas,'red');
+
+while 1:
+    ball.draw();
+    root.update();
+    time.sleep(0.01);
 
 
 
 
 
-root.mainloop();
+
+
+# root.mainloop();
