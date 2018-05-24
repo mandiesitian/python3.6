e16ad929814b6a8bedecd02b86be19ccd9756afc
mandiesitian <34275083+mandiesitian@users.noreply.github.com>
2018-05-24 20:26:12 +0800

    单线程单进程

--- Process/__init__.py
+++ Process/__init__.py
@@ -12,3 +12,33 @@ word 进程 可以进行打字、拼写检查没、打印等多个线程
 主线程可以创建子线程，子线程亦可以创建子线程
 
 '''
+#单线程
+'''
+单线程就是按照顺序执行程序的方式，只有当一个子任务完成以后才执行后面的任务
+
+'''
+
+from time import ctime,sleep;
+
+def music():
+    for i in range(2):
+        print('I am listen music {}'.format(ctime()));
+        sleep(1);
+
+
+def word():
+    for i in range(2):
+        print('I an write word{}'.format(ctime()));
+        sleep(1);
+
+
+
+music();
+word();
+
+'''
+使用for  循环2次  每次休眠1秒
+顺序执行，先执行music（） 在执行word（）  线程的存在必须依赖于进程
+此段代码为单进程单线程  
+
+'''
