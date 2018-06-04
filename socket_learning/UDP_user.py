27eb86f7c296558c134aaef9da9e05aa16864fe5
mandiesitian <34275083+mandiesitian@users.noreply.github.com>
2018-06-04 16:14:02 +0800

    UDP_user

--- socket_learning/UDP_user.py
+++ socket_learning/UDP_user.py
@@ -1,7 +1,9 @@
 #_*_ coding:utf-8 _*_;
 import socket;
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM);
-data='Jack',
+# for data in ['Jack','Tome']:
+
+data=input('请输入要发送的内容：');
 s.sendto(bytes(data,encoding='utf-8'),('127.0.0.1',10000));
 c=s.recvfrom(1024);
 print(c);
